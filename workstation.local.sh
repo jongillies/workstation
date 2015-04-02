a22c9515a87cb74f301aa05c3ce29de2cc16d7ed
Jon Gillies <jgillies@jgillies-2.local>
2015-04-02 15:25:07 -0700

    updates

--- workstation.local.sh
+++ workstation.local.sh
@@ -1,55 +1,69 @@
 #!/bin/bash
 
+####################
 # Virus Protection
+####################
 
 # brew_cask_install 'avast'
 
-# brew_cask_install 'sublime-text3'
-
-# brew_cask_install 'virtualbox'
-# brew_cask_install 'vagrant'
-# brew_cask_install 'vagrant-manager'
-
-# brew_cask_install 'boot2docker'
-# brew_cask_install 'boot2docker-status'
-
-# brew_cask_install 'atom'
-# brew_cask_install 'cloud'
-# brew_cask_install 'iterm2'
-
-# Browser Related
+####################
+# Browser
+####################
 
 # brew_cask_install 'firefox'
 # brew_cask_install 'google-chrome'
 # brew_cask_install 'lastpass'
 # brew_cask_install 'flash-player'
 
-# Virtual Machine Related
-# brew_cask_install 'vmware-fusion'
+####################
+# VM
+####################
 
+# brew_cask_install 'vmware-fusion'
+# brew_cask_install 'virtualbox'
+# brew_cask_install 'vagrant'
+# brew_cask_install 'vagrant-manager'
+# brew_cask_install 'boot2docker'
+# brew_cask_install 'boot2docker-status'
+
+####################
 # Cloud Services
+####################
+
 # brew_cask_install 'google-drive'
 # brew_cask_install 'dropbox'
 # brew_cask_install 'evernote'
 
+####################
 # Chat
+####################
 
 # brew_cask_install 'skype'
 # brew_cask_install 'adium'
 
+####################
 # Developer Tools
+####################
 
 # brew_cask_install 'rubymine'
 # brew_cask_install 'pycharm-ce'
 # brew_cask_install 'intellij-idea-ce'
 # brew_cask_install 'sourcetree'
+# brew_cask_install 'sublime-text3'
+# brew_cask_install 'atom'
+# brew_cask_install 'cloud'
+# brew_cask_install 'iterm2'
 
+####################
 # Screen grabbers
+####################
 
 # brew_cask_install 'jing'
 # brew_cask_install 'snagit'
 
-# Download
+####################
+# Download Helpers
+####################
 
 # brew_cask_install 'cyberduck'
 # brew_cask_install 'transmission'
@@ -57,7 +71,9 @@
 # brew_cask_install 'panic-unison'
 # brew_cask_install 'macpar-deluxe'
 
+####################
 # Misc
+####################
 
 # brew_cask_install 'mysqlworkbench'
 # brew_cask_install 'apache-directory-studio'
